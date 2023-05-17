f8b24c0325ffb84855547f1fc0b3014d7aee19f4
Woogie95 <choisw950928@gmail.com>
2023-05-17 22:21:38 +0900

    InitialCommit

---
+++ src/main/java/com/practice/pyeonhan_sesang/PyeonhanSesangApplication.java
@@ -0,0 +1,13 @@
+package com.practice.pyeonhan_sesang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyeonhanSesangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyeonhanSesangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/pyeonhan_sesang/PyeonhanSesangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.pyeonhan_sesang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyeonhanSesangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
