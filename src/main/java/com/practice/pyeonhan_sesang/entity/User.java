38357e90f376770e1c738d5b27d9d96ebe8b2256
Woogie95 <choisw950928@gmail.com>
2023-05-19 00:04:40 +0900

    feat: 유저 엔티티 생성

---
+++ src/main/java/com/practice/pyeonhan_sesang/entity/User.java
@@ -0,0 +1,34 @@
+package com.practice.pyeonhan_sesang.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user_table")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private String address;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+
+
+}
