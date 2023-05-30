9c389a5e41dac600ff6f686c99b1451f83f96bb4
Woogie95 <choisw950928@gmail.com>
2023-05-30 21:44:03 +0900

    refactor: UpdateDiaryRequest 필드 타입 수정, 생성자 어노테이션 추가

--- src/main/java/com/practice/pyeonhan_sesang/dto/request/UpdateDiaryRequest.java
+++ src/main/java/com/practice/pyeonhan_sesang/dto/request/UpdateDiaryRequest.java
@@ -1,20 +1,23 @@
 package com.practice.pyeonhan_sesang.dto.request;
 
 import com.practice.pyeonhan_sesang.entity.Diary;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateDiaryRequest {
 
     private String title;
     private String content;
     private String weather;
-    private String updated_at;
+    private LocalDateTime updated_at;
 
     public Diary toEntity() {
         return Diary.builder()
